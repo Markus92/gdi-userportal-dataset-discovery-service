27925672fea99b103b57e511fdf59bd0693c57c4
Mark Janse <mark.janse@health-ri.nl>
2024-08-13 17:06:13 +0200

    fix: contact point is now single valued again on CKAN side
    But we keep API compatible :)
    
    Note: tests still fail for the null case:
    
    ```
    field/property 'contacts' differ:
    - actual value  : [class ContactPoint {
        name: null
        email: null
        uri: null
    }]
    - expected value: []
    ```

--- src/test/java/io/github/genomicdatainfrastructure/discovery/services/PackageShowMapperTest.java
+++ src/test/java/io/github/genomicdatainfrastructure/discovery/services/PackageShowMapperTest.java
@@ -5,7 +5,6 @@
 package io.github.genomicdatainfrastructure.discovery.services;
 
 import io.github.genomicdatainfrastructure.discovery.model.*;
-import io.github.genomicdatainfrastructure.discovery.remote.ckan.model.CkanContactPoint;
 import io.github.genomicdatainfrastructure.discovery.remote.ckan.model.CkanCreator;
 import io.github.genomicdatainfrastructure.discovery.remote.ckan.model.CkanDatasetDictionaryEntry;
 import io.github.genomicdatainfrastructure.discovery.remote.ckan.model.CkanDatasetRelationEntry;
@@ -83,6 +82,8 @@ class PackageShowMapperTest {
                                 .name("en")
                                 .build()))
                 .contactUri("contactUri")
+                .contactEmail("contactEmail")
+                .contactName("contactName")
                 .hasVersion(List.of(
                         CkanValueLabel.builder()
                                 .displayName("version")
@@ -114,37 +115,22 @@ class PackageShowMapperTest {
                                 .uri("uri")
                                 .created("2024-03-19T13:37:05.472970")
                                 .lastModified("2024-03-19T13:37:05.472970")
-                                .build()
-                ))
-                .contactPoint(List.of(
-                        CkanContactPoint.builder()
-                                .contactName("Contact 1")
-                                .contactEmail("contact1@example.com")
-                                .build(),
-                        CkanContactPoint.builder()
-                                .contactName("Contact 2")
-                                .contactEmail("contact2@example.com")
-                                .contactUri("http://example.com")
-                                .build()
-                ))
+                                .build()))
                 .creator(List.of(
                         CkanCreator.builder()
                                 .creatorName("creatorName")
                                 .creatorIdentifier("creatorIdentifier")
-                                .build()
-                ))
+                                .build()))
                 .datasetRelationships(List.of(
                         CkanDatasetRelationEntry.builder().target("Dataset 1").relation(
                                 "Relation 1").build(),
                         CkanDatasetRelationEntry.builder().target("Dataset 2").relation(
-                                "Relation 2").build()
-                ))
+                                "Relation 2").build()))
                 .dataDictionary(List.of(
                         CkanDatasetDictionaryEntry.builder().name("Entry 1").type("Type 1")
                                 .description("Description 1").build(),
                         CkanDatasetDictionaryEntry.builder().name("Entry 2").type("Type 2")
-                                .description("Description 2").build()
-                ))
+                                .description("Description 2").build()))
                 .build();
 
         var actual = PackageShowMapper.from(ckanPackage);
@@ -157,8 +143,7 @@ class PackageShowMapperTest {
                         ValueLabel.builder()
                                 .value("theme-name")
                                 .label("theme")
-                                .build()
-                ))
+                                .build()))
                 .publisherName("publisherName")
                 .catalogue("organization")
                 .createdAt(parse("2024-03-19T13:37:05.472970", DATE_FORMATTER))
@@ -168,24 +153,17 @@ class PackageShowMapperTest {
                         ValueLabel.builder()
                                 .value("en")
                                 .label("language")
-                                .build()
-                ))
-                .contact(ValueLabel.builder()
-                        .value("contactUri")
-                        .label("contactUri")
-                        .build())
+                                .build()))
                 .hasVersions(List.of(
                         ValueLabel.builder()
                                 .value("1")
                                 .label("version")
-                                .build()
-                ))
+                                .build()))
                 .creators(List.of(
                         ValueLabel.builder()
                                 .label("creatorName")
                                 .value("creatorIdentifier")
-                                .build()
-                ))
+                                .build()))
                 .accessRights(ValueLabel.builder()
                         .value("public")
                         .label("accessRights")
@@ -194,8 +172,7 @@ class PackageShowMapperTest {
                         ValueLabel.builder()
                                 .value("conforms")
                                 .label("conformsTo")
-                                .build()
-                ))
+                                .build()))
                 .organization(DatasetOrganization
                         .builder()
                         .title("organization")
@@ -221,33 +198,33 @@ class PackageShowMapperTest {
                                         .label("format")
                                         .build())
                                 .uri("uri")
-                                .createdAt(parse("2024-03-19T13:37:05.472970", DATE_FORMATTER))
-                                .modifiedAt(parse("2024-03-19T13:37:05.472970", DATE_FORMATTER))
-                                .build()
-                ))
+                                .createdAt(parse("2024-03-19T13:37:05.472970",
+                                        DATE_FORMATTER))
+                                .modifiedAt(parse("2024-03-19T13:37:05.472970",
+                                        DATE_FORMATTER))
+                                .build()))
                 .contacts(List.of(
                         ContactPoint.builder()
-                                .name("Contact 1")
-                                .email("contact1@example.com")
-                                .build(),
-                        ContactPoint.builder()
-                                .name("Contact 2")
-                                .email("contact2@example.com")
-                                .uri("http://example.com")
-                                .build()
-                ))
+                                .name("contactName")
+                                .email("contactEmail")
+                                .uri("contactUri")
+                                .build()))
                 .datasetRelationships(List.of(
-                        DatasetRelationEntry.builder().relation("Relation 1").target("Dataset 1")
+                        DatasetRelationEntry.builder().relation("Relation 1")
+                                .target("Dataset 1")
                                 .build(),
-                        DatasetRelationEntry.builder().relation("Relation 2").target("Dataset 2")
-                                .build()
-                ))
+                        DatasetRelationEntry.builder().relation("Relation 2")
+                                .target("Dataset 2")
+                                .build()))
                 .dataDictionary(List.of(
-                        DatasetDictionaryEntry.builder().name("Entry 1").type("Type 1").description(
-                                "Description 1").build(),
-                        DatasetDictionaryEntry.builder().name("Entry 2").type("Type 2").description(
-                                "Description 2").build()
-                ))
+                        DatasetDictionaryEntry.builder().name("Entry 1").type("Type 1")
+                                .description(
+                                        "Description 1")
+                                .build(),
+                        DatasetDictionaryEntry.builder().name("Entry 2").type("Type 2")
+                                .description(
+                                        "Description 2")
+                                .build()))
                 .build();
 
         assertThat(actual)
